925a1e9916c63c0a1e545193d925be836713b171
nicole-nxn <nicolenxn1234@gmail.com>
2026-06-26 11:03:36 +1000

    add i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,10 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'zh'],
+    routing: { prefixDefaultLocale: true },
+  },
   integrations: [icon()],
 });
